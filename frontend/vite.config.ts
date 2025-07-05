dfb475b38fe5dac1c0c4535f2186a243d76a006f
ryan huynh <ryanhuynh200604@gmail.com>
2025-07-04 19:59:59 -0700

    updated vite.config build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/2048/",
+  base: "/My2048/",
   build: {
     outDir: "../docs",
     emptyOutDir: true
